fc5e52788a047941f9bb7876ab4d15027aa6f2f8
cole-propach <colepropach@gmail.com>
2025-12-03 23:32:06 -0800

    can now detect checks on a given board

--- Assets/board_controller.cs
+++ Assets/board_controller.cs
@@ -21,6 +21,11 @@ public class board_controller : MonoBehaviour
         printBoard();
     }
 
+    bool isInBounds(int row, int col)
+    {
+        return row >= 0 && col >= 0 && row <= 7 && col <= 7;
+    }
+
     void populateBoard()
     {
         //place pawns
@@ -148,10 +153,157 @@ public class board_controller : MonoBehaviour
     bool isInCheck(Piece[,] b, Color color)
     {
         Color opponentColor = (color == Color.White) ? Color.Black : Color.White;
-        //check every move of the opponents color and see if any of those result in capturing our king (dont have to be legal, can check with pinned pieces)
-        bool result = false;
+        //scan in a bishop, rook, knight, pawn, and king move away from our king
+        //if the specific piece is found, we must be in check
+        bool kingFound = false;
+        //find the square our king is on
+        int kingRow = 0, kingCol = 0;
+        for (kingRow = 0; kingRow < 8; kingRow++)
+        {
+            for (kingCol = 0; kingCol < 8; kingCol++)
+            {
+                if(b[kingRow, kingCol] == ((color == Color.White) ? Piece.WhiteKing : Piece.BlackKing))
+                {
+                    kingFound = true;
+                    break;
+                }
+            }
+            if (kingFound)
+            {
+                break;
+            }
+        }
+        //check a pawn capture away (color dependent) for enemy pawns
+        int colorSign = (opponentColor == Color.White) ? -1 : 1;
+        //if not out of bounds, check for pawns
+        if (!((kingRow + colorSign) > 7 || (kingRow + colorSign) < 0))
+        {
+            //check left side if in bounds
+            if (!((kingCol - 1) > 7 || (kingCol - 1) < 0))
+            {
+                if (b[kingRow + colorSign, kingCol - 1] == ((opponentColor == Color.White) ? Piece.WhitePawn : Piece.BlackPawn))
+                {
+                    return true;
+                }
+            }
+            //check right side if in bounds
+            if (!((kingCol + 1) > 7 || (kingCol + 1) < 0))
+            {
+                if (b[kingRow + colorSign, kingCol + 1] == ((opponentColor == Color.White) ? Piece.WhitePawn : Piece.BlackPawn))
+                {
+                    return true;
+                }
+            }
+        }
 
-        return result;
+        //check a king move away for an enemy king
+        for (int row = -1; row < 2; row++)
+        {
+            //break if row is out of bounds
+            if ((kingRow + row) > 7 || (kingRow + row) < 0) break;
+            for (int col = -1; col < 2; col++)
+            {
+                //break if col is out of bounds
+                if ((kingCol + col) > 7 || (kingCol + col) < 0) break;
+                if(b[(kingRow + row), (kingCol + col)] == ((opponentColor == Color.White) ? Piece.WhiteKing : Piece.BlackKing))
+                {
+                    return true;
+                }
+            }
+        }
+        //check a knight move away for enemy knights
+        int[] knightMoves = { -2, -1, 1, 2 };
+        foreach (int dr in knightMoves)
+        {
+            foreach (int dc in knightMoves)
+            {
+                if (Math.Abs(dr) + Math.Abs(dc) == 3) //valid knight L-shape
+                {
+                    int nr = kingRow + dr;
+                    int nc = kingCol + dc;
+
+                    if (nr >= 0 && nr < 8 && nc >= 0 && nc < 8)
+                    {
+                        if (b[nr, nc] == ((opponentColor == Color.White) ? Piece.WhiteKnight : Piece.BlackKnight))
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+
+        //check each diagonal until we hit a piece
+        //if that piece is a bishop or queen of the opposite color, return true
+        int[] diagDr = { -1, -1, 1, 1 };
+        int[] diagDc = { -1, 1, -1, 1 };
+
+        for (int d = 0; d < 4; d++)
+        {
+            int r = kingRow + diagDr[d];
+            int c = kingCol + diagDc[d];
+
+            while (r >= 0 && r < 8 && c >= 0 && c < 8)
+            {
+                Piece p = b[r, c];
+
+                if (p != Piece.Empty)
+                {
+                    if (p == ((opponentColor == Color.White) ? Piece.WhiteBishop : Piece.BlackBishop) || p == ((opponentColor == Color.White) ? Piece.WhiteQueen : Piece.BlackQueen))
+                    {
+                        return true;
+                    }
+                    break; //blocked by another piece
+                }
+
+                r += diagDr[d];
+                c += diagDc[d];
+            }
+        }
+
+        //check each file until we hit a piece
+        //if that piece is a rook or queen of the opposite color, return true
+        int[] direction = { -1, 1};
+
+        for (int d = 0; d < 2; d++)
+        {
+            //check the rows in the direction
+            int i = kingRow + direction[d];
+            while (i >= 0 && i < 8)
+            {
+                Piece p = b[i, kingCol];
+                if (p != Piece.Empty)
+                {
+                    if (p == ((opponentColor == Color.White) ? Piece.WhiteRook : Piece.BlackRook) || p == ((opponentColor == Color.White) ? Piece.WhiteQueen : Piece.BlackQueen))
+                    {
+                        return true;
+                    }
+                    break; //blocked
+                }
+
+                i += direction[d];
+            }
+
+            //check the cols in the direction
+            i = kingCol + direction[d];
+            while (i >= 0 && i < 8)
+            {
+                Piece p = b[kingRow, i];
+                if (p != Piece.Empty)
+                {
+                    if (p == ((opponentColor == Color.White) ? Piece.WhiteRook : Piece.BlackRook) || p == ((opponentColor == Color.White) ? Piece.WhiteQueen : Piece.BlackQueen))
+                    {
+                        return true;
+                    }
+                    break; //blocked
+                }
+
+                i += direction[d];
+            }
+        }
+
+        //return false if we didnt find any checks
+        return false;
     }
 
     List<Move> generateMoves(Color color)
@@ -206,6 +358,11 @@ public class board_controller : MonoBehaviour
             }
 
         };
+
+        //for every cell on the board
+            //append the moves of that cell
+        //for every move in moves
+            //if we make that move on an example board and it results in check for our color, remove it from the list
 
         return moves;
     }
